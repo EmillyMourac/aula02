35ae5a0eae00df6a0fd164dac689e56e52ce7cbb
EMILLY MOURA CARVALHO <emilly.carvalho@portalsesisp.org.br>
2026-02-19 14:20:03 -0300

    entregando ex 4, 5, 6 e 7

---
+++ exercicio4.cs
@@ -0,0 +1,16 @@
+using System.Reflection.Metadata;
+
+public static class Exercicio4
+{
+    public static void Executar()
+    {
+       string nome = " emilly moura";
+       string idade = "16";
+       string passatempo = "fazer musica,ir para a academia";
+       string faculdade = "fonoaudiologia";
+       Console.WriteLine($"meu nome é {nome} tenho {idade} anos, meus dois passatempo prefaridos são {passatempo} e pretendo cursar a graduação em {faculdade}");
+    }
+}
+
+
+    
---
+++ exercicio5.cs
@@ -0,0 +1,11 @@
+public static class Exercicio5
+{
+    public static void Executar()
+    {
+       string sou = " emilly moura";
+       string musicaquemaisgosto = "é bem ecletico";
+       string artistapreferido = "luan santana";
+       Console.WriteLine($"eu sou {sou}, meu estilo musical {musicaquemaisgosto}, mais meu artista preferido é {artistapreferido}");
+    }
+}
+
