f4c0e0706d229929d27fe443760828a8fe608760
EMILLY MOURA CARVALHO <emilly.carvalho@portalsesisp.org.br>
2026-02-19 11:33:41 -0300

    entregando ex 1, 2 e 3

---
+++ exercicio2.cs
@@ -0,0 +1,13 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+   // private static string nome = "emilly moura";
+    
+    public static void Executar()
+    {
+        string nome = "emilly moura";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
+
